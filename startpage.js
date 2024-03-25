125506565cfe9eb10690234fdaf6941dc5472883
evilweasel <38261180+EvilWeasel@users.noreply.github.com>
2024-03-25 15:39:04 +0100

    Startpage done

--- startpage.js
+++ startpage.js
@@ -1,1 +1,24 @@
-console.log("test");
+// Get Reference to HTML-Element from the DOM
+// DOM => Document-Object-Modell
+// DOM => Abstraction of HTML-Document
+const clockElem = document.getElementById("clock");
+
+clockElem.innerText = "Buxtehude";
+
+function updateClock() {
+    const date = new Date();
+    clockElem.innerText = date.toLocaleTimeString();
+}
+// C# Thread.Sleep(1000) => 1 Sekunde Delay
+setInterval(updateClock, 1000);
+
+
+// Chuck Norris Jokes
+const jokeElem = document.getElementById("joke");
+
+
+fetch("https://api.chucknorris.io/jokes/random?category=dev").then(response => {
+    return response.json();
+}).then(jsonData => {
+    jokeElem.innerText = jsonData.value;
+});
